7be0f92622b609354d06636f3f7d5f2d2dabf0c5
J.R. Maingat <jrmaingat@gmail.com>
2017-12-01 00:12:19 -0500

    Refactor drt sms receiver to its own class

--- app/src/main/java/com/tagniam/drtsms/ScheduleService.java
+++ app/src/main/java/com/tagniam/drtsms/ScheduleService.java
@@ -11,6 +11,8 @@ import android.telephony.SmsMessage;
 
 public class ScheduleService extends IntentService {
     public static final String SCHEDULE_RECEIVED = "schedule_received";
+
+    private DrtSmsReceiver drtSmsReceiver;
     private final String drtPhoneNumber = "8447460497";
 
     public ScheduleService() {
@@ -24,28 +26,49 @@ public class ScheduleService extends IntentService {
         SmsManager smsSender = SmsManager.getDefault();
         smsSender.sendTextMessage(drtPhoneNumber, null, stopId, null, null);
 
-        BroadcastReceiver msgReceiver = new BroadcastReceiver() {
-            @Override
-            public void onReceive(Context context, Intent intent) {
-                if (Telephony.Sms.Intents.SMS_RECEIVED_ACTION.equals(intent.getAction())) {
-                    for (SmsMessage smsMessage : Telephony.Sms.Intents.getMessagesFromIntent(intent)) {
-                        String from = smsMessage.getOriginatingAddress();
+        // Wait for sms to be received
+        registerDrtSmsReceiver();
+    }
 
-                        // Only analyze if from DRT
-                        // Note: apparently all SMS from DRT < 160 char, so don't need to concatenate msgs
-                        if (from.equals(drtPhoneNumber)) {
-                            String msg = smsMessage.getMessageBody();
+    /**
+     * Registers a BroadcastReceiver and waits for the DRT sms to be received.
+     */
+    private void registerDrtSmsReceiver() {
+        drtSmsReceiver = new DrtSmsReceiver();
+        IntentFilter intentFilter = new IntentFilter(Telephony.Sms.Intents.SMS_RECEIVED_ACTION);
+        getApplicationContext().registerReceiver(drtSmsReceiver, intentFilter);
+    }
 
-                            Intent resultIntent = new Intent();
-                            resultIntent.setAction(SCHEDULE_RECEIVED);
-                            resultIntent.putExtra("result", msg);
-                            sendBroadcast(resultIntent);
-                        }
+    /**
+     * Used to execute code on receiving the SMS from DRT.
+     */
+    private class DrtSmsReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (Telephony.Sms.Intents.SMS_RECEIVED_ACTION.equals(intent.getAction())) {
+                for (SmsMessage smsMessage : Telephony.Sms.Intents.getMessagesFromIntent(intent)) {
+                    String from = smsMessage.getOriginatingAddress();
+
+                    // Only analyze if from DRT
+                    // Note: apparently all SMS from DRT < 160 char, so don't need to concatenate msgs
+                    if (from.equals(drtPhoneNumber)) {
+                        broadcastSchedule(smsMessage.getMessageBody());
+                        break;
                     }
                 }
             }
-        };
+        }
 
-        getApplicationContext().registerReceiver(msgReceiver, new IntentFilter(Telephony.Sms.Intents.SMS_RECEIVED_ACTION));
+        /**
+         * Sends a broadcast containing the schedule information.
+         * @param msg the text msg response from DRT
+         */
+        private void broadcastSchedule(String msg) {
+            // TODO refactor to send a BusTime object
+            Intent resultIntent = new Intent();
+            resultIntent.setAction(SCHEDULE_RECEIVED);
+            resultIntent.putExtra("result", msg);
+            sendBroadcast(resultIntent);
+        }
     }
 }
