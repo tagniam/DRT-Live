f764c09d6fee360efc287b7ab9aabbacc2ad9f86
J.R. Maingat <jrmaingat@gmail.com>
2018-02-12 19:21:45 -0500

    Finally got this freaking db to work!!

--- app/src/main/java/com/tagniam/drtsms/database/GtfsRoomDatabase.java
+++ app/src/main/java/com/tagniam/drtsms/database/GtfsRoomDatabase.java
@@ -2,7 +2,9 @@ package com.tagniam.drtsms.database;
 
 import android.arch.persistence.db.SupportSQLiteDatabase;
 import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
+import android.arch.persistence.room.migration.Migration;
 import android.content.Context;
 import android.os.AsyncTask;
 import android.support.annotation.NonNull;
@@ -15,45 +17,20 @@ import com.tagniam.drtsms.database.routes.RouteDao;
  * Created by jr on 28/12/17.
  */
 
-@Database(entities = {Route.class}, version = 2)
+@Database(entities = {Route.class}, version = 1)
 public abstract class GtfsRoomDatabase extends RoomDatabase {
 
   private static final String DB_NAME = "gtfs.db";
   private static GtfsRoomDatabase instance;
-  private static RoomDatabase.Callback sRoomDatabaseCallback = new RoomDatabase.Callback() {
-
-    @Override
-    public void onOpen(@NonNull SupportSQLiteDatabase db) {
-      new PopulateDbAsync(instance).execute();
-    }
-
-  };
 
   public static GtfsRoomDatabase getDatabase(final Context context) {
     if (instance == null) {
-      instance = RoomAsset.databaseBuilder(context.getApplicationContext(), GtfsRoomDatabase.class,
-          DB_NAME).build();
+      instance = Room.databaseBuilder(context.getApplicationContext(), GtfsRoomDatabase.class,
+          DB_NAME).openHelperFactory(new AssetSQLiteOpenHelperFactory()).build();
     }
     return instance;
   }
 
   public abstract RouteDao routeDao();
-
-  private static class PopulateDbAsync extends AsyncTask<Void, Void, Void> {
-
-    private final RouteDao mDao;
-
-    PopulateDbAsync(GtfsRoomDatabase db) {
-      mDao = db.routeDao();
-    }
-
-    @Override
-    protected Void doInBackground(final Void... params) {
-      mDao.deleteAll();
-      mDao.insert(new Route("Brock","3","ffffff","006633", null, "112_merged_992564", null, null, "112"));
-
-      return null;
-    }
-  }
 
 }
