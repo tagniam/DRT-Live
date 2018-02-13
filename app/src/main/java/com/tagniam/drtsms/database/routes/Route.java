0a31d5bfc955ddf788b9f5bce1e19223ac4c1518
J.R. Maingat <jrmaingat@gmail.com>
2018-02-12 19:29:12 -0500

    Formatting

--- app/src/main/java/com/tagniam/drtsms/database/routes/Route.java
+++ app/src/main/java/com/tagniam/drtsms/database/routes/Route.java
@@ -13,34 +13,17 @@ import android.support.annotation.NonNull;
 @Entity(tableName = "routes")
 public class Route {
 
-  @PrimaryKey
-  @NonNull
-  @ColumnInfo(name = "route_id")
-  public String routeId;
+  @PrimaryKey @NonNull
+  @ColumnInfo(name = "route_id")          public String routeId;
 
-  @ColumnInfo(name = "route_long_name")
-  public String routeLongName;
-
-  @ColumnInfo(name = "route_type")
-  public String routeType;
-
-  @ColumnInfo(name = "route_text_color")
-  public String routeTextColor;
-
-  @ColumnInfo(name = "route_color")
-  public String routeColor;
-
-  @ColumnInfo(name = "agency_id")
-  public String agencyId;
-
-  @ColumnInfo(name = "route_url")
-  public String routeUrl;
-
-  @ColumnInfo(name = "route_desc")
-  public String routeDesc;
-
-  @ColumnInfo(name = "route_short_name")
-  public String routeShortName;
+  @ColumnInfo(name = "route_long_name")   public String routeLongName;
+  @ColumnInfo(name = "route_type")        public String routeType;
+  @ColumnInfo(name = "route_text_color")  public String routeTextColor;
+  @ColumnInfo(name = "route_color")       public String routeColor;
+  @ColumnInfo(name = "agency_id")         public String agencyId;
+  @ColumnInfo(name = "route_url")         public String routeUrl;
+  @ColumnInfo(name = "route_desc")        public String routeDesc;
+  @ColumnInfo(name = "route_short_name")  public String routeShortName;
 
   public Route(String routeLongName, String routeType, String routeTextColor,
       String routeColor, String agencyId, String routeId, String routeUrl, String routeDesc,
