054d4d1fe7d6df8d923ab9d4aad59f08bc759b17
J.R. Maingat <jrmaingat@gmail.com>
2018-04-01 18:47:46 -0400

    Show points on map

--- app/src/main/java/com/tagniam/drtsms/MapFragment.java
+++ app/src/main/java/com/tagniam/drtsms/MapFragment.java
@@ -1,16 +1,35 @@
 package com.tagniam.drtsms;
 
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+import com.tagniam.drtsms.database.GtfsRoomDatabase;
+import com.tagniam.drtsms.database.stops.Stop;
+import io.reactivex.Single;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.functions.Function;
+import io.reactivex.observers.DisposableSingleObserver;
+import io.reactivex.schedulers.Schedulers;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import org.osmdroid.api.IGeoPoint;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.simplefastpoint.LabelledGeoPoint;
+import org.osmdroid.views.overlay.simplefastpoint.SimpleFastPointOverlay;
+import org.osmdroid.views.overlay.simplefastpoint.SimpleFastPointOverlayOptions;
+import org.osmdroid.views.overlay.simplefastpoint.SimplePointTheme;
 
 public class MapFragment extends Fragment {
 
   private MapView map;
+  private SimpleFastPointOverlayOptions pointOptions;
 
   @Override
   public void onCreate(Bundle savedInstanceState) {
@@ -32,5 +51,80 @@ public class MapFragment extends Fragment {
     map.setTileSource(TileSourceFactory.MAPNIK);
     map.setBuiltInZoomControls(true);
     map.setMultiTouchControls(true);
+
+    // Setup point style
+    Paint pointStyle = new Paint();
+    pointStyle.setStyle(Paint.Style.FILL);
+    pointStyle.setColor(Color.parseColor("#48C873"));
+
+    // Setup point options
+    pointOptions = SimpleFastPointOverlayOptions.getDefaultStyle()
+        .setAlgorithm(SimpleFastPointOverlayOptions.RenderingAlgorithm.MAXIMUM_OPTIMIZATION)
+        .setRadius(7).setIsClickable(true).setCellSize(15).setPointStyle(pointStyle);
+
+    // Setup map points
+    setupMapPoints();
   }
+
+  private void setupMapPoints() {
+    Single.fromCallable(new Callable<List<Stop>>() {
+      @Override
+      public List<Stop> call() {
+        // Query database for stops
+        return GtfsRoomDatabase.getDatabase(getView().getContext())
+            .stopDao().loadAllStops();
+      }
+    }).map(new Function<List<Stop>, List<IGeoPoint>>() {
+      @Override
+      public List<IGeoPoint> apply(List<Stop> stops) {
+        // Convert stops to points using co-ordinates and stop name
+        List<IGeoPoint> points = new ArrayList<>();
+        for (Stop stop : stops) {
+          points.add(new LabelledGeoPoint(stop.stopLat, stop.stopLon, stop.stopName));
+        }
+        return points;
+      }
+    }).subscribeOn(Schedulers.io())
+        .observeOn(AndroidSchedulers.mainThread())
+        .subscribe(new DisposableSingleObserver<List<IGeoPoint>>() {
+          @Override
+          public void onSuccess(List<IGeoPoint> points) {
+            // Add points to map, using configured point style & options
+            SimplePointTheme pt = new SimplePointTheme(points, false);
+            SimpleFastPointOverlay pointsOverlay = new SimpleFastPointOverlay(pt, pointOptions);
+
+            pointsOverlay.setOnClickListener(new SimpleFastPointOverlay.OnClickListener() {
+              @Override
+              public void onClick(SimpleFastPointOverlay.PointAdapter points, Integer point) {
+                Toast.makeText(map.getContext()
+                    , ((LabelledGeoPoint) points.get(point)).getLabel()
+                    , Toast.LENGTH_SHORT).show();
+              }
+            });
+
+            map.getOverlays().add(pointsOverlay);
+          }
+
+          @Override
+          public void onError(Throwable e) {
+            Toast.makeText(getActivity().getApplicationContext(),
+                getActivity().getApplicationContext().getResources()
+                    .getString(R.string.error_generic),
+                Toast.LENGTH_SHORT).show();
+          }
+        });
+  }
+
+  @Override
+  public void onPause() {
+    super.onPause();
+    map.onPause();
+  }
+
+  @Override
+  public void onResume() {
+    super.onResume();
+    map.onResume();
+  }
+
 }
