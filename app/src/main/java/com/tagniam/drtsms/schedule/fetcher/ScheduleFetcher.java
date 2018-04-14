36daf3835992e852c2fd227f3fb5f0e88628c5c7
J.R. Maingat <jrmaingat@gmail.com>
2018-04-13 21:44:10 -0400

    Refactor more xml

--- app/src/main/java/com/tagniam/drtsms/schedule/fetcher/ScheduleFetcher.java
+++ app/src/main/java/com/tagniam/drtsms/schedule/fetcher/ScheduleFetcher.java
@@ -31,7 +31,7 @@ public abstract class ScheduleFetcher implements ObservableOnSubscribe<Intent> {
 
   public abstract void onResume();
 
-  private static final boolean DEBUG = true;
+  private static final boolean DEBUG = false;
 
   public static class Intents {
 
